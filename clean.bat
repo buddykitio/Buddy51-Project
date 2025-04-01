2ce2a683357a08339fbbed43d070448cc43c7060
Siva M Mac <msivamoorthy@gmail.com>
2025-04-01 21:21:41 +0530

    clean log removed on clean script

--- clean.bat
+++ clean.bat
@@ -5,8 +5,5 @@ rem Delete object and release directories if they exist
 if exist object rmdir /S /Q object
 if exist release rmdir /S /Q release
 
-rem Delete errors log
-if exist errors.log del errors.log
-
 echo Cleanup completed!
 exit /b 0
--- clean.sh
+++ clean.sh
@@ -5,8 +5,5 @@ echo "Cleaning build files..."
 # Remove object and release directories if they exist
 rm -rf object release
 
-# Remove errors log
-rm -f errors.log
-
 echo "Cleanup completed!"
 exit 0
